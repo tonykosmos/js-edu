bc6ed88da7cd28dc3cd496cd696498613b585769
tonykosmos <a.kosmovskiy@gmail.com>
2019-10-07 13:44:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+  focus = 'family', 
+  knowsProgramming = true,
+  config = {family: 4}
+  ) {
+    let hours = 0;
+    if (knowsProgramming == true) hours = 800;
+    else hours = 1300;
+    return Math.ceil(hours/config[focus]);      
+      
   };
   
